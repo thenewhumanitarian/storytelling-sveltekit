02eb988c16e4d7f3d4e7459e876be510deb8f900
Marc Fehr <marc.fehr@gmail.com>
2024-10-31 15:11:04 +0200

    Remove everything

--- src/lib/contentfulClient.js
+++
@@ -1,6 +0,0 @@
-import contentful from 'contentful';
-
-export const client = contentful.createClient({
-	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-	accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_API_TOKEN
-});
