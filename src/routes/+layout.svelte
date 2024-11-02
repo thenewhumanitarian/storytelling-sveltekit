dbf48c7fa75fadf09f70538fd44926377c0c065c
Marc Fehr <marc.fehr@gmail.com>
2024-11-02 12:39:14 +0200

    iframe resizer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,8 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<script type="text/javascript" src="/scripts/iframeResizer.contentWindow.min.js"></script>
-</svelte:head>
-
 <main data-iframe-height={true}>
 	{@render children()}
 </main>
+
+<script src="/scripts/iframeResizer.contentWindow.min.js" defer></script>
