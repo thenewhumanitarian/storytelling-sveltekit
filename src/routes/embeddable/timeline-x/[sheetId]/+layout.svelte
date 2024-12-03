27234c2931fed693d0d125263afa95e8ec2f9e1f
Marc Fehr <marc.fehr@gmail.com>
2024-12-03 15:56:58 +0200

    Styles for Knightlab Timeline

--- src/routes/embeddable/timeline-x/[sheetId]/+layout.svelte
+++ src/routes/embeddable/timeline-x/[sheetId]/+layout.svelte
@@ -10,3 +10,34 @@
 		<slot />
 	</main>
 </div>
+
+<style>
+	:global(
+			.tl-timeline .tl-caption,
+			.tl-timeline .tl-credit,
+			.tl-timeline .tl-slidenav-next,
+			.tl-timeline .tl-slidenav-previous
+		) {
+		font-size: 14px !important;
+		line-height: 17px !important;
+	}
+
+	:global(.tl-timeline p) {
+		font-size: 17px !important;
+		line-height: 1.25 !important;
+	}
+
+	:global(.tl-slide.tl-full-image-background a:hover, .tl-slide.tl-full-color-background a:hover) {
+		color: rgb(160, 60, 80) !important;
+	}
+
+	:global(.tl-medium .tl-text h2.tl-headline-title, .tl-medium .tl-text h2.tl-headline) {
+		font-size: 28px;
+		line-height: 32px;
+	}
+
+	:global(.tl-slide.tl-full-image-background .tl-caption) {
+		border-top: 1px solid white;
+		padding-top: 0.6rem;
+	}
+</style>
