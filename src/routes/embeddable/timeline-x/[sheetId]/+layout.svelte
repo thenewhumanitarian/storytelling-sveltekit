995c3fb76e868d0320788a2e37eb2fd80f518645
Marc Fehr <marc.fehr@gmail.com>
2025-02-12 14:06:51 +0200

    Remove Google font import for Roboto

--- src/routes/embeddable/timeline-x/[sheetId]/+layout.svelte
+++ src/routes/embeddable/timeline-x/[sheetId]/+layout.svelte
@@ -1,9 +1,9 @@
-<svelte:head>
+<!-- <svelte:head>
 	<link
 		rel="stylesheet"
 		href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
 	/>
-</svelte:head>
+</svelte:head> -->
 
 <div id="timeline" class="bg-zinc-50 font-body">
 	<main>
--- src/routes/embeddable/timeline-x/[sheetId]/+page.svelte
+++ src/routes/embeddable/timeline-x/[sheetId]/+page.svelte
@@ -1,41 +1,41 @@
 <script>
-  import { onMount } from 'svelte';
-  import { page } from '$app/stores';
-  import '@knight-lab/timelinejs/dist/css/timeline.css';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import '@knight-lab/timelinejs/dist/css/timeline.css';
 
-  let timelineContainer;
-  let sheetId;
+	let timelineContainer;
+	let sheetId;
 
-  $: sheetId = $page.params.sheetId; // Extract the sheetId from the URL
+	$: sheetId = $page.params.sheetId; // Extract the sheetId from the URL
 
-  onMount(() => {
-    import('@knight-lab/timelinejs').then(({ Timeline }) => {
-      const options = {
-        initial_zoom: 3,
-        timenav_position: 'bottom',
-        font: 'Roboto',
-        theme: 'timeline',
-      };
+	onMount(() => {
+		import('@knight-lab/timelinejs').then(({ Timeline }) => {
+			const options = {
+				initial_zoom: 3,
+				timenav_position: 'bottom',
+				font: 'Roboto',
+				theme: 'timeline'
+			};
 
-      new Timeline(
-        timelineContainer,
-        `https://docs.google.com/spreadsheets/d/${sheetId}/pubhtml`, // Dynamically use the sheetId
-        options
-      );
-    });
-  });
+			new Timeline(
+				timelineContainer,
+				`https://docs.google.com/spreadsheets/d/${sheetId}/pubhtml`, // Dynamically use the sheetId
+				options
+			);
+		});
+	});
 </script>
 
 <div id="timeline--wrapper" bind:this={timelineContainer}></div>
 
 <style>
-  #timeline--wrapper {
-    aspect-ratio: 1/1;
-  }
+	#timeline--wrapper {
+		aspect-ratio: 1/1;
+	}
 
-  @media screen and (max-width: 600px) {
-    #timeline--wrapper {
-      aspect-ratio: 1/2;
-    }
-  }
+	@media screen and (max-width: 600px) {
+		#timeline--wrapper {
+			aspect-ratio: 1/2;
+		}
+	}
 </style>
