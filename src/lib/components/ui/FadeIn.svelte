1f06dc90b820fd31875928ad10780f774af3bf7b
Marc Fehr <marc.fehr@gmail.com>
2025-02-12 09:49:07 +0200

    Custom fly in transitions

--- src/lib/components/ui/FadeIn.svelte
+++ src/lib/components/ui/FadeIn.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fly, blur } from 'svelte/transition';
 	import { inview } from 'svelte-inview';
 	import type { ObserverEventDetails, Options } from 'svelte-inview';
 	import { Spring } from 'svelte/motion';
@@ -7,38 +8,47 @@
 	export let duration: number = 500;
 	export let initialOpacity: number = 0.5;
 	export let finalOpacity: number = 1;
-	export let xOffset: number = -50; // Moves up on enter
-	// export let offset: string = '-10%'; // Root margin for in-view detection
+	export let xOffset: number = 0;
+	export let yOffset: number = 0;
+	export let blurAmount: number = 0;
+	export let inViewOffset: string = '0px';
 
 	let isInView: boolean = false;
 	const opacity = new Spring(initialOpacity, { stiffness: 0.2, damping: 0.8 });
 	const translateX = new Spring(xOffset, { stiffness: 0.2, damping: 0.5 });
 
 	const options: Options = {
-		// rootMargin: offset
+		rootMargin: inViewOffset
 	};
 
 	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
 		isInView = detail.inView;
+	};
 
-		if (isInView) {
-			opacity.target = finalOpacity;
-			translateX.target = 0;
-		} else {
-			opacity.target = initialOpacity;
-			translateX.target = xOffset;
-		}
+	const sharedOptions = {
+		duration: duration,
+		delay: delay
 	};
 </script>
 
-<div
-	use:inview={options}
-	on:inview_change={handleChange}
-	style="
-    opacity: {opacity.current};
-    transform: translateX({translateX.current}px);
-    transition: opacity {duration}ms ease-out {delay}ms, transform {duration}ms ease-out {delay}ms;
-  "
->
-	<slot />
+<div use:inview={options} on:inview_change={handleChange}>
+	{#if isInView}
+		<div
+			in:blur={{
+				...sharedOptions,
+				amount: blurAmount,
+				opacity: 0
+			}}
+		>
+			<div
+				in:fly={{
+					...sharedOptions,
+					x: xOffset,
+					y: yOffset
+				}}
+			>
+				<slot />
+			</div>
+		</div>
+	{/if}
 </div>
--- src/routes/ldd/home/+page.svelte
+++ src/routes/ldd/home/+page.svelte
@@ -2,34 +2,55 @@
 	import FadeIn from '$lib/components/ui/FadeIn.svelte';
 </script>
 
-<div></div>
-<div></div>
+<div>
+	<h1>Testing a few inView animations.</h1>
+</div>
+
 <div>
 	<FadeIn>
-		<h1>Hello, I fade in smoothly!</h1>
+		<p>This is the default without setting anything special.</p>
 	</FadeIn>
 </div>
 
 <div>
-	<FadeIn delay={200} duration={700}>
-		<p>This paragraph fades in slightly later.</p>
+	<FadeIn yOffset={50} blurAmount={10}>
+		<h1>Hello, I fade in smoothly! From the bottom.</h1>
 	</FadeIn>
 </div>
 
 <div>
-	<FadeIn yOffset={50} offset="-20%">
-		<button>Fading Button</button>
+	<FadeIn delay={1000} duration={1000} blurAmount={10}>
+		<p>This paragraph fades in slightly later – 200ms delay and 1,000ms duration...</p>
 	</FadeIn>
 </div>
-<div></div>
-<div></div>
+
+<div>
+	<FadeIn xOffset={500} blurAmount={10}>
+		<p>This here comes from the right (xOffset = 500).</p>
+	</FadeIn>
+</div>
+
+<div>
+	<FadeIn xOffset={-1000}>
+		<p>This here comes from the (far) left) (xOffset = -1000).</p>
+	</FadeIn>
+</div>
+
+<div>
+	<FadeIn blurAmount={10} xOffset={-50} yOffset={-50}>
+		<p>This here comes from the top left (xOffset = -50, yOffset = -50).</p>
+	</FadeIn>
+</div>
 
 <style>
 	div {
 		padding: 1rem;
 		height: 60vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 	div:nth-child(odd) {
-		background-color: #f0f0f0;
+		background-color: #ffee99;
 	}
 </style>
