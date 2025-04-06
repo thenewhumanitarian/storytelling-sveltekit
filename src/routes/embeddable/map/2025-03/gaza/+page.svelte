c8a2152382f985eec33e0938493d662c52287604
Radlantic <radlanticza@gmail.com>
2025-04-06 12:57:01 +0200

    add timeline

--- src/routes/embeddable/map/2025-03/gaza/+page.svelte
+++ src/routes/embeddable/map/2025-03/gaza/+page.svelte
@@ -1,8 +1,48 @@
 <script lang="ts">
-import GazaMap from "$lib/components/GazaMap/GazaMap.svelte";
+	import GazaMap from '$lib/components/GazaMap/GazaMap.svelte';
+	import Timeline from '$lib/components/GazaMap/Timeline.svelte';
+
+	// Shared state: ID of the selected incident
+	let selectedMarkerId = $state<number | null>(null);
+	let highlightedMarkerId = $state<number | null>(null);
+
+	function setSelectedMarkerId(id: number | null) {
+		selectedMarkerId = id;
+	}
+	function setHighlightedMarkerId(id: number | null) {
+		highlightedMarkerId = id;
+	}
 </script>
 
-<div class="">
-  <h1 class="text-2xl font-bold mb-4">Gaza Map</h1>
-  <GazaMap />
-</div>
+<main data-iframe-height={true}></main>
+
+<section class="relative flex h-screen w-full flex-col" data-iframe-height={true}>
+	<div class="flex-grow">
+		<!-- Map takes available space -->
+		<GazaMap
+			{selectedMarkerId}
+			{setSelectedMarkerId}
+			{highlightedMarkerId}
+			{setHighlightedMarkerId}
+		/>
+	</div>
+	<div class="absolute bottom-0 left-0 z-30 w-full">
+		<!-- Timeline container -->
+		<Timeline
+			{selectedMarkerId}
+			{setSelectedMarkerId}
+			{highlightedMarkerId}
+			{setHighlightedMarkerId}
+		/>
+	</div>
+</section>
+
+<svelte:head>
+	<style>
+		html,
+		body {
+			height: 100%;
+			margin: 0;
+		}
+	</style>
+</svelte:head>
