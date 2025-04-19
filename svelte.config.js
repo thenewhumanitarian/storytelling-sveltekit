776933d59158681933ff6ed41f461f24fd65b324
Marc Fehr <marc.fehr@gmail.com>
2025-04-19 19:46:49 +0200

    Vercel Adapter Change

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,10 +12,10 @@ const config = {
 	},
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: vercel(),
+		prerender: {
+			entries: ['*'] // or let dynamic ones be discovered via your entries() fn
+		}
 	}
 };
 
