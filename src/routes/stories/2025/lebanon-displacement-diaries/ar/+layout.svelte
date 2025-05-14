6e7f445e54344607a36b4b84532aa908f87ce51a
Marc Fehr <marc.fehr@gmail.com>
2025-05-14 21:39:22 +0200

    Remove noindex

--- src/routes/stories/2025/lebanon-displacement-diaries/ar/+layout.svelte
+++ src/routes/stories/2025/lebanon-displacement-diaries/ar/+layout.svelte
@@ -7,12 +7,12 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	<title>TNH| Title is missing</title>
 	<meta name="description" content="This is a prototype." />
 	<meta name="robots" content="noindex" />
-	<!-- Need to add more meta data here later -->
-</svelte:head>
+	Need to add more meta data here later
+</svelte:head> -->
 
 <div class="arabic" dir="rtl">
 	{@render children()}
