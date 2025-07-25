a49c79bf1c37357b2ba81aa641155802a2c37711
Marc Fehr <marc.fehr@gmail.com>
2025-07-25 15:04:48 +0200

    Adds Show Sources button

--- src/lib/components/gaza-map/GazaCard.svelte
+++ src/lib/components/gaza-map/GazaCard.svelte
@@ -11,13 +11,15 @@
 </script>
 
 <div
-	class={'${incident.chronoId === selectedMarkerId ? "bg-zinc-200" : ""} stack-cards__item js-stack-cards__item my-4 overflow-hidden border border-zinc-400 bg-white p-3 transition-colors duration-200 sm:mx-4 sm:cursor-default'}
+	class={'${incident.chronoId === selectedMarkerId ? "bg-zinc-200" : ""} stack-cards__item js-stack-cards__item my-4 flex flex-col justify-between overflow-hidden border border-zinc-400 bg-white p-3 transition-colors duration-200 sm:mx-4 sm:cursor-default'}
 	data-id={incident.chronoId}
 >
 	{#if incident.type === 'event'}
-		<div class="flex items-center justify-between gap-2 sm:gap-2">
+		<div
+			class={`flex items-start flex-col justify-start gap-2 sm:gap-2 ${incident.chronoId === selectedMarkerId ? 'opacity-100' : 'opacity-30'} transition-opacity duration-200`}
+		>
 			<div class="flex items-center gap-2">
-				<span class="px-2 py-1 text-sm font-bold text-white bg-burgundy">Event</span>
+				<span class="bg-burgundy px-2 py-1 text-sm font-bold text-white">Event</span>
 				<span class="text-sm text-gray-700">
 					{moment(incident.date).format('DD MMMM YYYY')}
 				</span>
@@ -41,21 +43,25 @@
 					→
 				</button>
 			</div>
+			<h3 class="mb-1 mt-2 line-clamp-1 text-lg font-bold leading-tight sm:text-xl">
+				{incident.title}
+			</h3>
+			{#if incident.description && incident.description.includes('<')}
+				<div class="hidden text-sm text-gray-700 sm:block sm:text-lg">
+					{@html incident.description}
+				</div>
+			{:else}
+				<p class="hidden text-sm text-gray-700 sm:block sm:text-lg">{incident.description}</p>
+			{/if}
+			<slot name="readmore"></slot>
 		</div>
-		<h3 class="mt-2 mb-1 text-lg font-bold leading-tight line-clamp-1 sm:text-xl">{incident.title}</h3>
-		{#if incident.description && incident.description.includes('<')}
-			<div class="hidden text-sm text-gray-700 sm:block sm:text-lg">
-				{@html incident.description}
-			</div>
-		{:else}
-			<p class="hidden text-sm text-gray-700 sm:block sm:text-lg">{incident.description}</p>
-		{/if}
-		<slot name="readmore"></slot>
 	{:else}
-		<div class={`${incident.chronoId === selectedMarkerId ? '' : 'sm:opacity-30'}`}>
+		<div
+			class={`${incident.chronoId === selectedMarkerId ? '' : 'sm:opacity-30'} transition-opacity duration-200`}
+		>
 			<div class="flex items-center justify-between gap-2 sm:gap-2">
 				<div class="flex items-center gap-2">
-					<span class="px-2 py-1 text-sm font-bold text-white bg-burgundy">Incident</span>
+					<span class="bg-burgundy px-2 py-1 text-sm font-bold text-white">Incident</span>
 					<span class="text-sm text-gray-700">
 						{moment(incident.date).format('DD MMMM YYYY')}
 					</span>
@@ -80,7 +86,9 @@
 					</button>
 				</div>
 			</div>
-			<h3 class="mt-2 mb-1 text-lg font-bold leading-tight line-clamp-1 sm:text-xl">{incident.title}</h3>
+			<h3 class="mb-1 mt-2 line-clamp-1 text-lg font-bold leading-tight sm:text-xl">
+				{incident.title}
+			</h3>
 			<h5 class="mb-2 text-sm italic text-gray-600 sm:text-base">
 				{incident.killedOrWounded} killed/wounded
 			</h5>
@@ -88,7 +96,7 @@
 			{#if incident.videoUrl}
 				<div class="hidden sm:block">
 					<video
-						class="relative w-full mt-2 rounded-lg aspect-video video"
+						class="aspect-video video relative mt-2 w-full rounded-lg"
 						src={incident.videoUrl}
 						controls
 						aria-label={incident.videoCaption || incident.title}
@@ -97,7 +105,7 @@
 			{:else if incident.imageUrl}
 				<div class="hidden sm:block">
 					<img
-						class="relative w-full mt-2 rounded-lg aspect-video video"
+						class="aspect-video video relative mt-2 w-full rounded-lg"
 						src={incident.imageUrl}
 						alt={incident.imageCaption || incident.title}
 					/>
@@ -121,6 +129,14 @@
 					{incident.videoCaption}
 				</p>
 			{/if}
+		</div>
+		<div
+			class={`mt-4 flex justify-start font-bold ${incident.chronoId === selectedMarkerId ? 'opacity-100' : 'opacity-30'} transition-opacity duration-200`}
+		>
+			<button
+				class="bg-zinc-900/80 px-2 py-1 text-sm text-white opacity-80 transition-opacity duration-200 hover:opacity-100"
+				>Show sources</button
+			>
 		</div>
 	{/if}
 </div>
