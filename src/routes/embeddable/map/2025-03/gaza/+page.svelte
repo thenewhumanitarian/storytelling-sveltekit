ce8cd32ab15157d15edd1267734cda363541d269
Marc Fehr <marc.fehr@gmail.com>
2025-09-11 14:20:01 +0200

    Put timeline back to bottom

--- src/routes/embeddable/map/2025-03/gaza/+page.svelte
+++ src/routes/embeddable/map/2025-03/gaza/+page.svelte
@@ -75,26 +75,28 @@
 
 <main class="h-screen bg-white">
 	<section class="flex flex-col w-full h-full min-h-0">
-		<div class="max-h-[40vh] min-h-[120px] flex-grow">
-			<GazaMap
-				bind:this={gazaMapRef}
-				{selectedMarkerId}
-				{setSelectedMarkerId}
-				{highlightedMarkerId}
-				{setHighlightedMarkerId}
-				{incidentsData}
-				{selectedWeekStartDate}
-			/>
+		<div class="flex-grow flex flex-col">
+			<div class="flex-grow min-h-[120px]">
+				<GazaMap
+					bind:this={gazaMapRef}
+					{selectedMarkerId}
+					{setSelectedMarkerId}
+					{highlightedMarkerId}
+					{setHighlightedMarkerId}
+					{incidentsData}
+					{selectedWeekStartDate}
+				/>
+			</div>
+			<div class="w-full flex-shrink-0">
+				<GazaCards
+					bind:this={gazaCardsRef}
+					{incidentsData}
+					{selectedMarkerId}
+					onCardInView={handleCardInView}
+				/>
+			</div>
 		</div>
-		<div class="w-full flex-shrink-0">
-			<GazaCards
-				bind:this={gazaCardsRef}
-				{incidentsData}
-				{selectedMarkerId}
-				onCardInView={handleCardInView}
-			/>
-		</div>
-		<div class="z-30 w-full">
+		<div class="z-30 w-full flex-shrink-0">
 			<Timeline
 				{setHighlightedMarkerId}
 				{highlightedMarkerId}
@@ -121,12 +123,10 @@
 			width: 100%;
 			height: 100%;
 			min-height: 120px;
-			max-height: 100svh;
 		}
 		@media (max-width: 640px) {
 			.map-container {
 				min-height: 120px;
-				max-height: 40vh;
 				height: 100%;
 			}
 		}
